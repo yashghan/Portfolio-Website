77162b8d9f6be4e52e09a408995d71ffaf56ffc8
yashghan <yashdghan@gmail.com>
2023-11-29 09:48:33 +0530

    Changes to Contact component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-
+import { useState, useEffect } from "react";
 
 function App() {
+  // const [theme, setTheme] = useState(null);
+
+  // useEffect(() => {
+  //   if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+  //     setTheme('dark');
+  //   }
+  //   else {
+  //     setTheme('light');
+  //   }
+  // }, [])
+
+  // useEffect(() => {
+  //   if (theme === "dark") {
+  //     document.documentElement.classList.add("dark");
+  //   } else {
+  //     document.documentElement.classList.remove("dark");
+  //   }
+  // }, [theme]);
+
+  // const handleThemeSwitch = () => {
+  //   setTheme(theme === "dark" ? "light" : "dark");
+  // };
+
   return (
     <div>
+
       <Navbar />
       <Home />
       <About />
